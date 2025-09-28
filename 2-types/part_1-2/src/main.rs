f1c2b95f878154b838baebbfa70d3a41b4cfc4b1
vregi <vreginalld@gmail.com>
2025-09-28 19:46:51 -0400

    feat(suict-rust-2025): complete part 2 of 2-types assignment

--- 2-types/part_1-2/src/main.rs
+++ 2-types/part_1-2/src/main.rs
@@ -1,6 +1,30 @@
-mod part_1;
+pub mod part_1;
 mod part_2;
 
+use serde::{Serialize, Deserialize};
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("/home/vregi/Desktop/rust-assignments/2-types/request.json");
+    let display = path.display();
+
+   let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => println!("{} contains:\n{}", display, s),
+    }
+
+    let deserialized: part_2::Request = serde_json::from_str(&s).unwrap();
+    println!("\ndeserialized = {:?}", deserialized);
+
+    let serialized: String = toml::to_string(&deserialized).unwrap();
+    println!("\nserialized = {}", serialized);
 }
