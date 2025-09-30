b84091ae13d338ee7a208f069dabc44ac89815d6
vregi <vreginalld@gmail.com>
2025-09-30 19:50:18 -0400

    fix(suict-rust-2025): fix hardcode path in part 1-2 main

--- 2-types/part_1-2/src/main.rs
+++ 2-types/part_1-2/src/main.rs
@@ -8,19 +8,7 @@ use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
-    let path = Path::new("/home/vregi/Desktop/rust-assignments/2-types/request.json");
-    let display = path.display();
-
-   let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => println!("{} contains:\n{}", display, s),
-    }
+    let s = include_str!("../../request.json");
 
     let deserialized: part_2::Request = serde_json::from_str(&s).unwrap();
     println!("\ndeserialized = {:?}", deserialized);
