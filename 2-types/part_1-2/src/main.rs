71e9946b36b8407c8d593c511121328137521da2
vregi <vreginalld@gmail.com>
2025-09-30 22:09:08 -0400

    chore(suict-rust-2025): remove unused dependencies in main

--- 2-types/part_1-2/src/main.rs
+++ 2-types/part_1-2/src/main.rs
@@ -1,12 +1,6 @@
 pub mod part_1;
 mod part_2;
 
-use serde::{Serialize, Deserialize};
-
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
 fn main() {
     let s = include_str!("../../request.json");
 
